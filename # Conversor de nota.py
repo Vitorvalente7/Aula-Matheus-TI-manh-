0f5ca14d72c2d406632d0c9a83afb22d85d6a47a
Vitorvalente7 <166523368+Vitorvalente7@users.noreply.github.com>
2024-04-09 20:31:28 -0300

    Add files via upload

---
+++ # Conversor de nota.py
@@ -0,0 +1,12 @@
+# Conversor de nota
+Nota=float(input("Digite sua nota: "))
+if Nota >=+9:
+ print( " A ")
+elif Nota >=+7.5:
+ print(" B ")
+elif Nota >=6:
+ print(" C ")
+elif Nota >=4:
+ print(" D ")
+else:
+ print(" F ")

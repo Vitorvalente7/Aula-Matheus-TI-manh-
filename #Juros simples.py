9b97aefb63f73be06e7fff67691d432401e7342b
Vitorvalente7 <166523368+Vitorvalente7@users.noreply.github.com>
2024-04-11 08:58:13 -0300

    Update # Juros simples.py

--- #Juros simples.py
+++ #Juros simples.py
@@ -1,6 +1,6 @@
-#Juros simples 
-c=float(input("Digite o capital : "))
-i=float(input("Digite a taxa de juros : "))
+# Juros simples 
+c=float(input("Digite o capital: "))
+i=float(input("Digite a taxa de juros: "))
 t=float(input("Digite o tempo : "))
 j=(c*i*t)
 print("O juros apos",(t),"anos sera de",format(c*i*t))

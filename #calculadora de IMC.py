e74deedf3e9df41bb7f10441393294dae05c0e49
Vitorvalente7 <166523368+Vitorvalente7@users.noreply.github.com>
2024-04-11 21:16:18 -0300

    Update #calculadora de IMC.py

--- #calculadora de IMC.py
+++ #calculadora de IMC.py
@@ -1,13 +1,13 @@
 #calculadora de IMC
 peso=float(input("Digite seu peso: "))
 altura=float(input("Digite sua altura: "))
-IMC= peso/(altura*altura)
-if IMC <18.5:
+IMC= peso/(altura * altura)
+if IMC < 18.5:
     print("Magreza !")
-elif IMC <=24.9:
+elif IMC <= 24.9:
     print("Normal !")
-elif IMC <=30:
+elif IMC <= 30:
     print("Sobrepeso !")
-elif IMC <=40:
+elif IMC <= 40:
     print("Obesidade !")
 

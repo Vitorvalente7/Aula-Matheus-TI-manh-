9bc2f187966026baa17d326012e661b8c9e1f525
Vitorvalente7 <166523368+Vitorvalente7@users.noreply.github.com>
2024-04-21 14:04:20 -0300

    Update #Calculadora de desconto !!.py

--- #Calculadora de desconto !!.py
+++ #Calculadora de desconto !!.py
@@ -1,5 +1,5 @@
 #Calculadora de desconto !!
-preco=float(input("digite o preço: "))
+preço=float(input("digite o preço: "))
 desconto=float(input("Digite o valor do desconto: "))
-f= preco - (preco * desconto / 100)
+f= preço - (preço * desconto / 100)
 print(" O desconto é R$ :",format (f))

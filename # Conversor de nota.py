f7954bbb3eb70be5f4d9c3ad66e3a6d6d5021c3b
Vitorvalente7 <166523368+Vitorvalente7@users.noreply.github.com>
2024-04-21 14:21:14 -0300

    Update # Conversor de nota.py

--- # Conversor de nota.py
+++ # Conversor de nota.py
@@ -4,9 +4,9 @@ if Nota >= +9:
  print( " A ")
 elif Nota >= +7.5:
  print(" B ")
-elif Nota >= 6:
+elif Nota >= +6:
  print(" C ")
-elif Nota >= 4:
+elif Nota >= +4:
  print(" D ")
 else:
  print(" F ")

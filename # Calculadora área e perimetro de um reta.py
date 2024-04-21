f62e5b20717d161c551a94d40afde1336e0385a4
Vitorvalente7 <166523368+Vitorvalente7@users.noreply.github.com>
2024-04-21 13:57:35 -0300

    Update # Calculadora área e perimetro de um reta.py

--- # Calculadora área e perimetro de um reta.py
+++ # Calculadora área e perimetro de um reta.py
@@ -1,6 +1,6 @@
-# Calculadora área e perimetro de um retangulo
+# Calculadora área e perímetro de um retangulo
 largura=float(input("Digite a largura: "))
 altura=float(input("Digite a altura: "))
-area= largura * altura
-perimetro= 2 * (largura + altura)
-print("O perimetro do retangulo é",(perimetro),"e a area é",(area))
+área= largura * altura
+perímetro= 2 * (largura + altura)
+print("O perímetro do retangulo é",(perímetro),"e a área é",(área))
